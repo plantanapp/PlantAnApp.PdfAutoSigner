96c8a9be2c0911b9c9d5acee89b59303f9e4b9ab
Razvan Ion <razvan.ion@plantanapp.com>
2022-06-06 15:45:33 +0300

    Create SignatureFactory

---
+++ PdfAutoSigner.Lib/Signatures/ISignatureFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace PdfAutoSigner.Lib.Signatures
+{
+    public interface ISignatureFactory
+    {
+        Pkcs11Signature CreatePkcs11Signature(string libraryPath, ulong slotId, string hashAlgorithm = "SHA256", string? alias = null, string? certLabel = null, string? pin = null);
+        X509Certificate2Signature CreateX509Certificate2Signature(string issuerName, ulong signatureIdx, string hashAlgorithm, string pin);
+    }
+}
---
+++ PdfAutoSigner.Lib/Signatures/SignatureFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace PdfAutoSigner.Lib.Signatures
+{
+    public class SignatureFactory : ISignatureFactory
+    {
+        public Pkcs11Signature CreatePkcs11Signature(string libraryPath, ulong slotId, string hashAlgorithm = "SHA256", string? alias = null, string? certLabel = null, string? pin = null)
+        {
+            return new Pkcs11Signature(libraryPath, slotId)
+                .Select(alias, certLabel, pin).SetHashAlgorithm(hashAlgorithm);
+        }
+
+        public X509Certificate2Signature CreateX509Certificate2Signature(string issuerName, ulong signatureIdx, String hashAlgorithm, string pin)
+        {
+            var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.MaxAllowed);
+            var certificates = store.Certificates.Where(x => x.HasPrivateKey && x.Issuer.ToLower().Contains(issuerName.ToLower())).ToArray();
+            var certificate = certificates[signatureIdx];
+            store.Close();
+
+            return new X509Certificate2Signature(certificate, hashAlgorithm).Select(pin);
+        }
+    }
+}
